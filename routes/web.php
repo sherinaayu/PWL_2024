51d7587b055f82c43f79d1dda7ee37efe6bf128f
sherinaayu <144317837+sherinaayu@users.noreply.github.com>
2024-02-22 13:38:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+Route::get('/hello', function () {
+ return 'Hello World';
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
- return 'Hello World';
-});
-
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
-});*/
+});
